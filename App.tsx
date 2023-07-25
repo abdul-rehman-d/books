b11ab9db0a4b6c2c5545db0f91da6091938420ec
Abdul Rehman <ar618211@gmail.com>
2023-07-26 03:07:36 +0500

    uninstalled react-query and made my own fetch hook

--- App.tsx
+++ App.tsx
@@ -2,17 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 
 import MainStack from './src/stacks/MainStack';
-import { QueryClient, QueryClientProvider } from 'react-query';
-
-const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <NavigationContainer>
-        <MainStack />
-        <StatusBar style="auto" />
-      </NavigationContainer>
-    </QueryClientProvider>
+    <NavigationContainer>
+      <MainStack />
+      <StatusBar style="auto" />
+    </NavigationContainer>
   );
 }
--- src/hooks/useBooks.ts
+++ src/hooks/useBooks.ts
@@ -1,16 +1,89 @@
-import { useQuery } from "react-query";
+import React, { useCallback, useEffect, useState } from "react";
 import api from "../services/api";
 import { parseBook } from "../utils/parsing";
 
-async function getAllBooks() {
-  const response = await api.get("/volumes?q=subject:inspirational&maxResults=20");
+const MAX_RESULTS = 20;
+
+async function getAllBooks(startIndex: number) {
+  const response = await api.get(
+    "/volumes?q=subject:inspirational" +
+    "&maxResults="+ MAX_RESULTS +
+    "&startIndex=" + startIndex
+  );
   if (response.status === 200) {
-    const books: Book[] = response.data.items.map(parseBook);
-    return books;
+    const books: Book[] = response.data.items
+      .map(parseBook)
+      .filter((book: Book) => Boolean(book.thumbnail));
+    return { books, totalItems: response.data.totalItems };
   }
-  return [];
+  return { books: [], totalItems: Number.MAX_SAFE_INTEGER };
 }
 
-const useBooks = () => useQuery<Book[], Error>("books", getAllBooks);
+const useBooks = () => {
+  const [ isLoading, setIsLoading ] = useState<boolean>(true);
+  const [ isRefreshing, setIsRefreshing ] = useState<boolean>(false);
+  const [ isFetchingNext, setIsFetchingNext ] = useState<boolean>(false);
+  const [ isError, setIsError ] = useState<boolean>(false);
+  const [ startIndex, setStartIndex ] = useState<number>(0);
+  const [ books, setBooks ] = useState<Book[]>([]);
+  const [ totalItems, setTotalItems ] = useState<number>(0);
+
+  const handleFetch = useCallback(
+    (
+      setLoading: React.Dispatch<React.SetStateAction<boolean>>,
+      nextStartingIndex: number
+    ) => {
+      setLoading(true);
+      getAllBooks(nextStartingIndex)
+        .then(
+          ({ books, totalItems }: { books: Book[]; totalItems: number }) => {
+            if (nextStartingIndex === 0) {
+              setBooks(books);
+            } else {
+              setBooks((curr) => [...curr, ...books]);
+            }
+            setStartIndex(nextStartingIndex + MAX_RESULTS);
+            setTotalItems(totalItems);
+            setLoading(false);
+          }
+        )
+        .catch(() => {
+          setLoading(false);
+          setIsError(true);
+        });
+    },
+    []
+  );
+
+
+  const fetchNext = useCallback(() => {
+    if (!isFetchingNext) {
+      console.log("fetching next", startIndex, totalItems);
+      if (startIndex < totalItems) {
+        handleFetch(setIsFetchingNext, startIndex);
+      }
+    }
+  }, [isFetchingNext, startIndex, totalItems])
+
+  const refresh = useCallback(() => {
+    console.log("refreshing");
+    handleFetch(setIsRefreshing, 0);
+  }, []);
+
+  useEffect(() => {
+    console.log("fetching first time");
+    handleFetch(setIsLoading, 0);
+  }, []);
+
+  return {
+    books,
+    isLoading,
+    isError,
+    isRefreshing,
+    isFetchingNext,
+    fetchNext,
+    refresh,
+  }
+};
 
 export default useBooks;
