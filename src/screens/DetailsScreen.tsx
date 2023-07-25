febeba5280e1815e922262053cedee878d773054
Abdul Rehman <ar618211@gmail.com>
2023-07-26 01:55:21 +0500

    list of book names with navigation to details page

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,12 +1,15 @@
-import { Text, View } from 'react-native'
+import { Image } from 'react-native'
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../types/stack';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 function DetailsScreen({ route, navigation }: NativeStackScreenProps<RootStackParamList, 'Details'>) {
+  const book = route.params.book;
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Image source={{ uri: book.thumbnail }} style={{ width: '100%', height: 300, objectFit: 'cover' }} />
+    </SafeAreaView>
   );
 }
 
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,49 @@
-import { Text, View } from 'react-native'
+import { ActivityIndicator, FlatList, Pressable, StyleSheet, Text, View } from 'react-native'
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../types/stack';
+import useBooks from '../hooks/useBooks';
 
 function HomeScreen({ route, navigation }: NativeStackScreenProps<RootStackParamList, 'Home'>) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+  const {
+    data: books,
+    isLoading,
+    isFetching,
+    isError,
+    refetch,
+  } = useBooks();
+
+  if (isLoading) return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ActivityIndicator />
     </View>
+  )
+
+  if (isError) return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Error</Text>
+    </View>
+  )
+
+  return (
+    <FlatList
+      data={books}
+      style={styles.container}
+      keyExtractor={item => item.id}
+      renderItem={({ item }) => (
+        <Pressable onPress={() => navigation.navigate('Details', { book: item })}>
+          <Text>{item.title}</Text>
+        </Pressable>
+      )}
+      onRefresh={refetch}
+      refreshing={isFetching}
+    />
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  }
+});
 
 export default HomeScreen;
