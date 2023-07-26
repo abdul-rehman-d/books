4e4eb62cc3ccde60632de10b40980f3ba82b39f9
Abdul Rehman <ar618211@gmail.com>
2023-07-26 12:28:28 +0500

    completed details screen

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,16 +1,138 @@
-import { Image } from 'react-native'
-import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../types/stack';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
+import StarRating from 'react-native-star-rating-widget';
+
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+import type { RootStackParamList } from '../types/stack';
+import { colors } from '../styles/theme';
 
 function DetailsScreen({ route, navigation }: NativeStackScreenProps<RootStackParamList, 'Details'>) {
   const book = route.params.book;
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Image source={{ uri: book.thumbnail }} style={{ width: '100%', height: 300, objectFit: 'cover' }} />
+    <SafeAreaView style={styles.container}>
+      {/* header */}
+      <Text style={styles.heading}>
+        {book.title}
+      </Text>
+      <View style={styles.publishLine}>
+        <Text style={styles.subtitle}>
+          <Text>{'Published from '}</Text>
+          <Text style={styles.subtitleDark}>{book.publisher}</Text>
+        </Text>
+        <Text style={styles.subtitle}>
+          <Text>{book.publishedDate}</Text>
+        </Text>
+      </View>
+
+      {/* hero */}
+      <View style={[styles.imageWrapper, styles.leftRoundedBorder]}>
+        <Image
+          source={{ uri: book.thumbnail }}
+          style={[styles.image, styles.leftRoundedBorder]}
+        />
+      </View>
+
+      {/* rating */}
+      <View style={styles.ratingWrapper}>
+        {
+          (book.ratingCount > 0 && book.rating > 0) && (
+            <View style={styles.ratingRow}>
+              <Text style={styles.ratingText}>{book.rating.toFixed(1)}</Text>
+              <StarRating
+                rating={book.rating}
+                onChange={()=>{}}
+                color='#FEB422'
+                starSize={20}
+                style={{ marginBottom: 5, marginLeft: 10 }}
+                starStyle={{ marginHorizontal: 1 }}
+                emptyColor='#eee'
+                enableSwiping={false}
+                animationConfig={{
+                  duration: 0,
+                  scale: 1,
+                }}
+              />
+            </View>
+          )
+        }
+        <Text style={styles.subtitle}>
+          {book.ratingCount ?? '0'} {book.ratingCount === 1 ? 'rating' : 'ratings'}
+        </Text>
+      </View>
+
+      {/* desc */}
+      <ScrollView>
+        <Text style={styles.description}>{book.description ?? 'No Description'}</Text>
+      </ScrollView>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  heading: {
+    fontSize: 40,
+    fontWeight: '600',
+    marginBottom: 10,
+    color: colors.secondary,
+  },
+  publishLine: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  subtitle: {
+    marginBottom: 10,
+    fontSize: 16,
+    color: '#444',
+    fontWeight: '300',
+  },
+  subtitleDark: {
+    fontWeight: '700',
+    color: colors.secondary,
+  },
+
+  imageWrapper: {
+    marginBottom: 20,
+    position: 'relative',
+    marginRight: -20,
+    height: 300,
+    width: '110%',
+    backgroundColor: '#f7f7f7',
+  },
+  image: {
+    width: '85%',
+    height: '100%',
+    objectFit: 'cover'
+  },
+  leftRoundedBorder: {
+    marginLeft: 'auto',
+    borderTopLeftRadius: 30,
+    borderBottomLeftRadius: 30,
+  },
+
+  ratingWrapper: {
+    marginBottom: 10,
+  },
+  ratingText: {
+    fontSize: 32,
+    color: colors.secondary,
+  },
+  ratingRow: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+  },
+
+  description: {
+    fontSize: 18,
+    lineHeight: 28,
+    color: colors.secondary,
+  }
+});
 
 export default DetailsScreen;
